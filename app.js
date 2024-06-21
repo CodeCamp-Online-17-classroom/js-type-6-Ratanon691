eb5d69ed10b21b1edc36ed5be26fc278ede0c9f1
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-21 10:17:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// write code here
+typeof 'x'; //string
+typeof x; //undefined
+typeof ``; //string
+typeof Infinity; //undefined
+typeof(100 / 0); //number
+typeof(1 + '3'); //string
+typeof(1 * 'a'); //number
+typeof null; //object
